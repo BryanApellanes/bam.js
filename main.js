5d1fd42da9fa883839f48b2d55e0e1d37b90c49b
Bryan Apellanes <bapellanes@vimly.com>
2019-09-23 15:00:12 -0700

    requireable main.js

---
+++ main.js
@@ -0,0 +1,50 @@
+/**
+ * A browserfiable main entry into bamjs
+ */
+
+module.exports = (function(){
+    return function(bamRootPath){
+        var colors = require("colors"),
+            path = require("path"),    
+            jsdom = require('jsdom'),
+            bamRoot = bamRootPath;
+        
+        const { JSDOM } = jsdom;
+        const jsDomObj = new JSDOM(``);
+        
+        if(typeof global === 'undefined'){
+            console.log('global not defined; instantiating as object'.yellow);
+            global = {};
+        }
+        
+        if(typeof window === 'undefined'){
+            console.log('window not defined; setting');
+            window = jsDomObj.window;
+        }
+        
+        if(typeof document === 'udnefined'){
+            console.log('document not defined; setting');
+            document = window.document;
+        }
+        
+        var jQuery = require("jquery");
+        
+        global.document = document;
+        global.jQuery = jQuery;
+        
+        function bamRequire(subPath, globalName){
+            var fullPath = path.join(bamRoot, subPath);
+            console.log(`bamRequire: ${path}`.yellow);
+            var module = require(fullPath);
+            if(typeof global !== 'undefined' && globalName){
+                global[globalName] = module;
+            }
+            return module;
+        }
+        
+        window.qi = bamRequire("/data/qi.js", "qi");
+        window.sdo = bamRequire("/data/sdo.js", "sdo");
+        window.dao = bamRequire("/data/dao.js", "dao");
+        window.bam = bamRequire("/bam.js", "bam")({});
+    }
+})();
